c88d0bae545aa4d05ce69969eeee48efe31c751c
liza <gorbatiukliza221009@gmail.com>
2026-09-16 15:30:41 +0300

    Lab02 Task08

---
+++ Lab02/Task8.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Lab02
+{
+    internal class Task8
+    {
+        public static void Run()
+        {
+            Console.Write("Введіть кількість відділень: ");
+            int d = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Введіть кількість тижнів: ");
+            int w = int.Parse(Console.ReadLine()!);
+
+            int[,,] patients = new int[d, w, 2];
+
+            for (int i = 0; i < d; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    for (int k = 0; k < 2; k++)
+                    {
+                        Console.Write($"Відділення {i + 1}, тиждень {j + 1}, " + $"{(k == 0 ? "ранок" : "вечір")}: ");
+                        patients[i, j, k] = int.Parse(Console.ReadLine()!);
+                    }
+                }
+            }
+
+            int[] totals = new int[d];
+
+            for (int i = 0; i < d; i++)
+            {
+                Console.WriteLine($"Відділення {i + 1}:");
+
+                for (int j = 0; j < w; j++)
+                {
+                    int morning = patients[i, j, 0];
+                    int evening = patients[i, j, 1];
+
+                    int weekTotal = morning + evening;
+                    totals[i] += weekTotal;
+
+                    Console.WriteLine(
+                        $"  Тиждень {j + 1}: ранок {morning}, " +
+                        $"вечір {evening} → разом {weekTotal}");
+                }
+
+                Console.WriteLine($"Разом: {totals[i]} пацієнтів");
+            }
+
+            int maxIndex = 0;
+
+            for(int i = 1; i < d; i++)
+            {
+                if (totals[i] > totals[maxIndex])
+                {
+                    maxIndex = i;
+                }
+            }
+
+            Console.WriteLine($"Найзавантаженіше: Відділення {maxIndex + 1} " +
+                $"({totals[maxIndex]} пацієнтів)");
+
+
+        }
+    }
+}
