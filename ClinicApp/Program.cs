459caf5bf3d817f3704d7e773ea9ba4804873c2c
liza <gorbatiukliza221009@gmail.com>
2026-09-23 19:46:41 +0300

    Lab03 Task01

--- ClinicApp/Program.cs
+++ ClinicApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ClinicApp;
+
+internal class Program
+{
+    static void Main()
+    {
+        Patient[] patients =
+        {
+            new Patient("Іван", "Петренко",
+                        DateTime.Today.AddYears(-41), "A+", "0501234567"),
+            new Patient("Олена", "Коваль",
+                        DateTime.Today.AddYears(-33), "B-", "0672345678"),
+            new Patient("Максим", "Бойко",
+                        DateTime.Today.AddYears(-16), "O+", "0933456789"),
+            new Patient(),
+            new Patient("Марія", "Ткач")
+        };
+
+        foreach (Patient patient in patients)
+        {
+            Console.WriteLine(patient);
+        }
+    }
+}
